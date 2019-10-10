19e60133023347a135c6519bac3c7a6c793d7ebc
axigamer <55416567+axigamer@users.noreply.github.com>
2019-10-10 13:58:31 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/u2_a2/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.u2_a2
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
